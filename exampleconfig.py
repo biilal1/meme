ecf11920819bcfc020009b5facc7a0a8a48571cf
sedthon2440 <166177796+sedthon2440@users.noreply.github.com>
2024-07-19 15:03:10 +0300

    Update exampleconfig.py

--- exampleconfig.py
+++ exampleconfig.py
@@ -15,7 +15,7 @@ class Development(Config):
     # command handler
     COMMAND_HAND_LER = "."
     # sudo enter the id of sudo users userid's in that array
-    SUDO_USERS = []
+    
     # command hanler for sudo
     SUDO_COMMAND_HAND_LER = "."
     TZ = "Asia/Baghdad" 
